1a94c5cf36897494161e0a700ce4853ad10cba5c
Jon1Games <118659471+Jon1Games@users.noreply.github.com>
2024-05-27 14:29:45 +0200

    Add files via upload

---
+++ src/main/java/de/jonas/griefpreventionsendblocks/GriefPreventionSendBlocks.java
@@ -0,0 +1,17 @@
+package de.jonas.griefpreventionsendblocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GriefPreventionSendBlocks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
